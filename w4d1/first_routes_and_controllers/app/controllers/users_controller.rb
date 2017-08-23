3c7dedef957ac8311e8dc708df736d7a150b7327
mwei210 <m.wei210@gmail.com>
2017-08-22 22:18:24 -0700

    Finished controller

--- w4d1/first_routes_and_controllers/app/controllers/users_controller.rb
+++ w4d1/first_routes_and_controllers/app/controllers/users_controller.rb
@@ -4,11 +4,11 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.new(user_params)
-    if user.save!
-      render json: user
+    @user = User.new(user_params)
+    if @user.save!
+      render json: @user
     else
-      render json: user.errors.full_messages, status:
+      render json: @user.errors.full_messages, status:
       :unprocessable_entity
     end
   end
--- w4d1/first_routes_and_controllers/db/schema.rb
+++ w4d1/first_routes_and_controllers/db/schema.rb
@@ -16,6 +16,11 @@ ActiveRecord::Schema.define(version: 20170821231850) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "email", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email"
   end
 
 end
